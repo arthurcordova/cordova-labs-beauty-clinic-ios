3e9441e46b929c6f07cae42553c594041adab26b
Arthur Cordova <arthur.stapassoli@gmail.com>
2017-02-25 00:29:13 -0300

    Drawer menu

---
+++ Beauty Clinic 2.0/Beauty Clinic 2.0/DrawerMenuController.swift
@@ -0,0 +1,20 @@
+//
+//  DrawerMenuController.swift
+//  Beauty Clinic 2.0
+//
+//  Created by Arthur on 25/02/17.
+//  Copyright © 2017 Cordova labs. All rights reserved.
+//
+
+import UIKit
+
+class DrawerMenuController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    @IBAction func closeAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Beauty Clinic 2.0/Beauty Clinic 2.0/SegueFromLeft.swift
@@ -0,0 +1,27 @@
+//
+//  SegueFromLeft.swift
+//  Beauty Clinic 2.0
+//
+//  Created by Arthur on 25/02/17.
+//  Copyright © 2017 Cordova labs. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class SegueFromLeft: UIStoryboardSegue {
+    
+    override func perform() {
+        let src: UIViewController = self.source
+        let dst: UIViewController = self.destination
+        let transition: CATransition = CATransition()
+        let timeFunc : CAMediaTimingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.duration = 0.25
+        transition.timingFunction = timeFunc
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromLeft
+        src.navigationController!.view.layer.add(transition, forKey: kCATransition)
+        src.navigationController!.pushViewController(dst, animated: false)
+    }
+    
+}
