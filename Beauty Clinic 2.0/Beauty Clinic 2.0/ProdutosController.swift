e3897588cb46cf297d503c5579c3a673e12422dc
Arthur Cordova <arthur.stapassoli@gmail.com>
2017-03-08 01:03:34 -0300

    Filter products

--- Beauty Clinic 2.0/Beauty Clinic 2.0/ProdutosController.swift
+++ Beauty Clinic 2.0/Beauty Clinic 2.0/ProdutosController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class ProdutosController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ProdutosController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchBarDelegate {
     
-    @IBOutlet var buttonSearch: UIBarButtonItem!
     @IBOutlet var tableProdutos: UITableView!
     
     var mainController: PrincipalController?
     var produtos : Array<Produto> = []
+    var filteredProducts : Array<Produto> = []
     var pessoa : Pessoa? = nil
     var model: Produto?
     var principalController: PrincipalController?
+    var searchController :UISearchController!
     
     override func viewDidLoad() {
         tableProdutos.delegate = self
@@ -30,37 +31,84 @@ class ProdutosController: UIViewController, UITableViewDelegate, UITableViewData
         
         loadProdutos()
         
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Pesquisar procedimento..."
+        searchController.searchBar.showsCancelButton = false
+        searchController.searchBar.showsSearchResultsButton = false
+        searchController.searchBar.setShowsCancelButton(false, animated: false)
+        
+        
+        definesPresentationContext = true
+        tableProdutos.tableHeaderView = searchController.searchBar
+        
+    }
+
+    func didPresentSearchController(searchController: UISearchController) {
+        searchController.searchBar.showsCancelButton = false
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredProducts = self.produtos.filter { p in
+            return p.descricao.lowercased().contains(searchText.lowercased())
+        }
+        tableProdutos.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredProducts.count
+        }
         return produtos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:MensagemCellTableViewCell = self.tableProdutos.dequeueReusableCell(withIdentifier: "cellProduto") as! MensagemCellTableViewCell
         cell.selectionStyle = UITableViewCellSelectionStyle.none
-        let produto = produtos[indexPath.row]
+        
+        let prod : Produto
+        if searchController.isActive && searchController.searchBar.text != "" {
+            prod = filteredProducts[indexPath.row]
+        } else {
+            prod = produtos[indexPath.row]
+        }
+        
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
         formatter.locale = NSLocale(localeIdentifier: "pt_BR") as Locale!
         
-        cell.labelTitulo.text = produto.descricao
-        cell.labelMensagem.text = formatter.string(from: produto.valorProduto)!
+        cell.labelTitulo.text = prod.descricao
+        cell.labelMensagem.text = formatter.string(from: prod.valorProduto)!
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var pro: Produto!
+        
+        if searchController.isActive {
+            pro = filteredProducts[indexPath.row]
+            searchController.isActive = false
+        } else {
+            pro = produtos[indexPath.row]
+        }
         
         OperationQueue.main.addOperation {
-            let addAlerta = UIAlertController(title: "Carrinho", message: "Deseja adicionar o item \(self.produtos[indexPath.row].descricao!) ao carrinho ?", preferredStyle: UIAlertControllerStyle.alert)
+            let addAlerta = UIAlertController(title: "Carrinho", message: "Deseja adicionar o item \(pro.descricao!) ao carrinho ?", preferredStyle: UIAlertControllerStyle.alert)
             
             addAlerta.addAction(UIAlertAction(title: "Sim", style: .default, handler: { (action: UIAlertAction!) in
-                self.principalController?.produtosCarrinho.append(self.produtos[indexPath.row])
+                self.principalController?.produtosCarrinho.append(pro)
             }))
             
             addAlerta.addAction(UIAlertAction(title: "Não", style: .cancel, handler: nil))
             self.present(addAlerta, animated: true, completion: nil)
             self.tableProdutos.deselectRow(at: indexPath, animated:true)
+
         }
     }
     
